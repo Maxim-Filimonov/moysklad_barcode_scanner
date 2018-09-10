a9a45e9c6d6e9ae511ff082ba88159c79805cdb4
Maxim Filimonov <tpaktopsp@gmail.com>
2018-09-10 16:08:25 +0300

    Simple version in getting barcodes in separate array for now.

--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,20 @@
-module Main exposing (main)
+port module Main exposing (main)
+
+-- PORTS
 
 import Base64
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-import Http
-import Json.Decode as Decode
+import Http exposing (toTask)
+import Json.Decode as Decode exposing (array, field, list, map2, string)
+import Json.Encode as E
+import List exposing (map)
+import Task exposing (Task, perform, sequence)
+
+
+port setToken : String -> Cmd msg
 
 
 
@@ -31,14 +39,23 @@ type alias Model =
     , password : String
     , token : String
     , loggedIn : Bool
+    , products : List String
+    , productBarcodes : List (List String)
     }
 
 
-init : () -> ( Model, Cmd Msg )
-init _ =
-    ( Model "" "" "" False
-    , Cmd.none
-    )
+init : Maybe String -> ( Model, Cmd Msg )
+init authToken =
+    case authToken of
+        Just token ->
+            ( Model "" "" token True [] []
+            , Cmd.none
+            )
+
+        Nothing ->
+            ( Model "" "" "" False [] []
+            , Cmd.none
+            )
 
 
 type Msg
@@ -46,6 +63,9 @@ type Msg
     | PasswordChange String
     | GetToken
     | TokenCheck (Result Http.Error String)
+    | LoadReport
+    | SalesReport (Result Http.Error (List String))
+    | ProductData (Result Http.Error (List (List String)))
 
 
 
@@ -84,14 +104,86 @@ update msg model =
             )
 
         TokenCheck (Ok product) ->
-            ( { model | loggedIn = True }, Cmd.none )
+            ( { model | loggedIn = True }, setToken model.token )
 
         TokenCheck (Err _) ->
             ( model, Cmd.none )
 
+        LoadReport ->
+            ( model
+            , Http.send SalesReport <|
+                Http.request
+                    { method = "GET"
+                    , headers =
+                        [ Http.header "Authorization" model.token
+                        ]
+                    , url = reportSalesByVariant
+                    , body = Http.emptyBody
+                    , expect = Http.expectJson productCodeEncoder
+                    , timeout = Nothing
+                    , withCredentials = False
+                    }
+            )
+
+        SalesReport (Ok linksToProducts) ->
+            ( { model | products = linksToProducts }
+            , Task.attempt ProductData (sequence (prepareRequests model.token linksToProducts))
+            )
+
+        SalesReport (Err err) ->
+            ( model, Cmd.none )
+
+        ProductData (Ok productBarcodes) ->
+            ( { model | productBarcodes = productBarcodes }
+            , Cmd.none
+            )
+
+        ProductData (Err err) ->
+            ( model, Cmd.none )
+
+
+prepareRequests : String -> List String -> List (Task Http.Error (List String))
+prepareRequests token links =
+    map toTask (requestForEachLink token links)
+
+
+requestForEachLink : String -> List String -> List (Http.Request (List String))
+requestForEachLink token links =
+    map (hasBarcode token) links
+
+
+hasBarcode token ref =
+    Http.request
+        { method = "GET"
+        , headers =
+            [ Http.header "Authorization" token
+            ]
+        , url = getProxiedUrl ref
+        , body = Http.emptyBody
+        , expect = Http.expectJson barCodeEncoder
+        , timeout = Nothing
+        , withCredentials = False
+        }
+
 
 getProducts =
     getProxiedUrl "https://online.moysklad.ru/api/remap/1.1/entity/product"
+
+
+barCodeEncoder =
+    field "barcodes" (list string)
+
+
+reportSalesByVariant =
+    getProxiedUrl "https://online.moysklad.ru/api/remap/1.1/report/sales/byvariant"
+
+
+productCodeEncoder =
+    field "rows" (list referenceDecoder)
+
+
+referenceDecoder =
+    field "assortment" (field "meta" (field "href" string))
 
 
 getProxiedUrl : String -> String
@@ -138,7 +230,13 @@ view model =
             ]
 
     else
-        h1 [] [ text "You are logged in" ]
+        div []
+            [ h1 []
+                [ text "You are logged in" ]
+            , button
+                [ onClick LoadReport ]
+                [ text "Load Report" ]
+            ]
 
 
 buildAuthorizationToken : String -> String -> String
