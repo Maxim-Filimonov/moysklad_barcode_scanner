a1059ee3fdfd53e5fae3a647ddae539628acff2f
Maxim Filimonov <tpaktopsp@gmail.com>
2018-09-28 17:06:43 +0300

    Omg it works with update. Styling time

--- src/Main.elm
+++ src/Main.elm
@@ -9,7 +9,7 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http exposing (toTask)
-import Json.Decode as Decode exposing (Decoder, andThen, array, dict, field, list, map, map2, map3, string, succeed)
+import Json.Decode as Decode exposing (Decoder, andThen, array, dict, field, list, map, map2, map3, map4, string, succeed)
 import Json.Encode as E
 import Process exposing (spawn)
 import Task exposing (Task, perform, sequence)
@@ -44,6 +44,7 @@ type alias Model =
     , products : Report
     , err : Maybe Http.Error
     , page : Int
+    , detailsRequests : Int
     }
 
 
@@ -51,12 +52,12 @@ init : Maybe String -> ( Model, Cmd Msg )
 init authToken =
     case authToken of
         Just token ->
-            ( Model "" "" token True Dict.empty Nothing 1
+            ( Model "" "" token True Dict.empty Nothing 1 0
             , Cmd.none
             )
 
         Nothing ->
-            ( Model "" "" "" False Dict.empty Nothing 1
+            ( Model "" "" "" False Dict.empty Nothing 1 0
             , Cmd.none
             )
 
@@ -65,9 +66,14 @@ type Msg
     = LoginChange String
     | PasswordChange String
     | GetToken
+    | SaveBarCode String
     | TokenCheck (Result Http.Error String)
     | LoadReport
     | ProductDataLoaded (Result Http.Error Report)
+    | ProductDetailsLoaded (Result Http.Error ProductDetails)
+    | ReportLoaded (Result Http.Error Report)
+    | UpdateProductBarcode String String
+    | BarcodeUpdated (Result Http.Error ProductDetails)
 
 
 
@@ -113,13 +119,7 @@ update msg model =
 
         LoadReport ->
             ( model
-            , Http.toTask (loadReport model.token model.page)
-                |> Task.andThen
-                    (\report ->
-                        Task.map (addProductDetailsResultsToReport report) (Task.sequence (prepareRequests model.token report))
-                    )
-                |> Task.map filterOnlyMissingBarcodes
-                |> Task.attempt ProductDataLoaded
+            , Http.send ReportLoaded (loadReport model.token model.page)
             )
 
         ProductDataLoaded (Ok report) ->
@@ -128,11 +128,70 @@ update msg model =
         ProductDataLoaded (Err err) ->
             ( model, Cmd.none )
 
+        ProductDetailsLoaded (Ok details) ->
+            if model.detailsRequests == 1 then
+                ( { model
+                    | detailsRequests = 0
+                    , products = filterOnlyMissingBarcodes (addProductDetails details model.products)
+                  }
+                , Cmd.none
+                )
+
+            else
+                ( { model
+                    | detailsRequests = model.detailsRequests - 1
+                    , products = addProductDetails details model.products
+                  }
+                , Cmd.none
+                )
+
+        ProductDetailsLoaded (Err err) ->
+            ( model, Cmd.none )
+
+        ReportLoaded (Ok report) ->
+            ( { model
+                | products = Dict.union model.products report
+                , detailsRequests = Dict.size report
+              }
+            , loadDetailsForReport model report
+                |> Cmd.batch
+            )
+
+        ReportLoaded (Err error) ->
+            ( { model | err = Just error }, Cmd.none )
+
+        SaveBarCode productCode ->
+            let
+                productDetails =
+                    Dict.get productCode model.products
+                        |> Maybe.andThen .details
+            in
+            ( model, updateBarcode model.token productDetails )
+
+        BarcodeUpdated _ ->
+            ( model, Cmd.none )
+
+        UpdateProductBarcode productCode newBarcode ->
+            ( { model | products = Dict.update productCode (updateBarcodes newBarcode) model.products }, Cmd.none )
+
+
+updateBarcode : String -> Maybe ProductDetails -> Cmd Msg
+updateBarcode token productDetails =
+    case productDetails of
+        Just val ->
+            Http.send BarcodeUpdated (sendBarcodeUpdate token val)
+
+        Nothing ->
+            Cmd.none
+
 
 addProductDetailsResultsToReport : Report -> List ProductDetails -> Report
 addProductDetailsResultsToReport report productDetails =
     List.foldl addProductDetails report productDetails
 
+
+
+-- addProductDetailsToTasks: Report -> List Task Http.Error ProductDetails -> List Task Http.Error
 
 
 addProductDetailsToReport : Report -> Task Http.Error ProductDetails -> Task Http.Error Report
@@ -142,6 +201,12 @@ addProductDetailsToReport report detailsTask =
 
 type alias Report =
     Dict String ReportRow
+
+
+loadDetailsForReport : Model -> Report -> List (Cmd Msg)
+loadDetailsForReport model report =
+    prepareRequests model.token report
+        |> List.map (Task.attempt ProductDetailsLoaded)
 
 
 loadMore : Model -> Report -> ( Model, Cmd Msg )
@@ -164,6 +229,7 @@ loadMore model report =
         ( model, Cmd.none )
 
 
+loadReport : String -> Int -> Http.Request Report
 loadReport token page =
     Http.request
         { method = "GET"
@@ -198,6 +264,16 @@ addProductDetails productDetails products =
     Dict.update productDetails.code (updateProductDetails productDetails) products
 
 
+updateBarcodes : String -> Maybe ReportRow -> Maybe ReportRow
+updateBarcodes barcode reportRow =
+    Maybe.map (\row -> { row | details = updateBarcodeWithinProductDetails barcode row.details }) reportRow
+
+
+updateBarcodeWithinProductDetails : String -> Maybe ProductDetails -> Maybe ProductDetails
+updateBarcodeWithinProductDetails barcode productDetails =
+    Maybe.map (\details -> { details | barcodes = [ barcode ] }) productDetails
+
+
 updateProductDetails : ProductDetails -> Maybe ReportRow -> Maybe ReportRow
 updateProductDetails productDetails reportRow =
     Maybe.map (\row -> { row | details = Just productDetails }) reportRow
@@ -230,22 +306,48 @@ getProducts =
 
 barCodeEncoder : Decode.Decoder ProductDetails
 barCodeEncoder =
-    map3 ProductDetails
+    map4 ProductDetails
         (field "code" string)
         (field "barcodes" (list string))
         (field "name" string)
+        (field "id" string)
 
 
 type alias ProductDetails =
     { code : String
     , barcodes : List String
     , name : String
+    , id : String
     }
 
 
 reportSalesByVariant : Int -> String
 reportSalesByVariant page =
     getApiUrl [ "report", "sales", "byvariant" ] (Just [ Url.Builder.string "offset" (String.fromInt (page * 25)) ])
+
+
+sendBarcodeUpdate : String -> ProductDetails -> Http.Request ProductDetails
+sendBarcodeUpdate token details =
+    Http.request
+        { method = "PUT"
+        , headers =
+            [ Http.header "Authorization" token
+            ]
+        , url = getApiUrl [ "entity", "product", details.id ] Nothing
+        , body = barcodeEncoderForReal details
+        , expect = Http.expectJson barCodeEncoder
+        , timeout = Nothing
+        , withCredentials = False
+        }
+
+
+barcodeEncoderForReal : ProductDetails -> Http.Body
+barcodeEncoderForReal productDetails =
+    Http.jsonBody
+        (E.object
+            [ ( "barcodes", E.list E.string productDetails.barcodes )
+            ]
+        )
 
 
 
@@ -273,14 +375,16 @@ type alias ReportRow =
     { code : String
     , href : String
     , details : Maybe ProductDetails
+    , id : Maybe String
     }
 
 
 reportRowDecoder : Decode.Decoder ReportRow
 reportRowDecoder =
-    map3 ReportRow
+    map4 ReportRow
         (field "code" string)
         (field "meta" (field "href" string))
+        (succeed Nothing)
         (succeed Nothing)
 
 
@@ -321,11 +425,17 @@ renderProduct : ReportRow -> Html Msg
 renderProduct product =
     case product.details of
         Just productDetails ->
-            if missingBarcodes productDetails.barcodes then
-                li [ style "color" "green" ] [ text productDetails.name ]
-
-            else
-                li [ style "color" "red" ] [ text productDetails.name ]
+            let
+                barcode =
+                    Maybe.withDefault "" (List.head productDetails.barcodes)
+            in
+            li [ style "list-style" "none" ]
+                [ span [] [ text productDetails.name ]
+                , Html.form [ onSubmit (SaveBarCode productDetails.code) ]
+                    [ input [ onInput (UpdateProductBarcode productDetails.code), type_ "text", placeholder "Barcode", value barcode ] []
+                    , input [ type_ "submit" ] []
+                    ]
+                ]
 
         Nothing ->
             li [] []
@@ -363,8 +473,18 @@ view model =
             , button
                 [ onClick LoadReport ]
                 [ text "Load Report" ]
-            , ul [] (List.map renderProduct (Dict.values model.products))
+            , renderListOrLoading model.detailsRequests model.products
             ]
+
+
+renderListOrLoading : Int -> Report -> Html Msg
+renderListOrLoading detailsRequests report =
+    if detailsRequests > 1 then
+        div [ class "loading" ]
+            [ span [] [ text "Loading..." ] ]
+
+    else
+        ul [] (List.map renderProduct (Dict.values report))
 
 
 buildAuthorizationToken : String -> String -> String
