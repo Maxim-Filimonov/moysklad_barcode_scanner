f5985ac035ccbe58f7ac22a4d17677d55acac9d3
Maxim Filimonov <tpaktopsp@gmail.com>
2018-10-03 12:12:40 +0300

    remove plugin. not needed anymore

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import main from "./src/Main.elm";
-// import("./src/Main.elm").then(({ Elm }) => {
-//   var app = Elm.Main.init({
-//     node: document.getElementById("root"),
-//     flags: localStorage.getItem("token"),
-//   });
-//   app.ports.setToken.subscribe(function(token) {
-//     localStorage.setItem("token", token);
-//   });
-// });
+// import main from "./src/Main.elm";
+import("./src/Main.elm").then(({ Elm }) => {
+  var app = Elm.Main.init({
+    node: document.getElementById("root"),
+    flags: localStorage.getItem("token"),
+  });
+  app.ports.setToken.subscribe(function(token) {
+    localStorage.setItem("token", token);
+  });
+});
